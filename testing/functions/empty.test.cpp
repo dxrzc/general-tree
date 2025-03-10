1f308178e085c133bed99e04f924e9270e2987fc
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-10 16:40:07 -0500

    test: empty method

---
+++ testing/functions/empty.test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "general-tree.h"
+
+TEST(empty, returnTrueIfTreeIsEmpty)
+{
+	general_tree<int> test_tree;
+	EXPECT_TRUE(test_tree.empty());
+}
+
+TEST(empty, returnFalseIfTreeNotEmpty)
+{
+	general_tree<int> test_tree;
+	test_tree.create_root(1);
+	EXPECT_FALSE(test_tree.empty());
+}
