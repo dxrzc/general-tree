571ee57f2274877dad3c0967039a90c12bc26dc0
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-11 20:11:12 -0500

    test: node::child method

--- testing/classes/node.test.cpp
+++ testing/classes/node.test.cpp
@@ -10,7 +10,7 @@ TEST(Node, defaultNodeIsNull)
 // boolean operator
 
 TEST(Node, booleanOperator_returnTrueIfNodeIsNotNull)
-{	
+{
 	general_tree<int> tree(1);
 	general_tree<int>::node node = tree.root();
 	EXPECT_TRUE(node);
@@ -165,4 +165,67 @@ TEST(Node, isNull_ReturnFalseIfNodeIsNotNull)
 {
 	general_tree<int> test_tree(1);
 	EXPECT_FALSE(test_tree.root().is_null());
+}
+
+// child
+
+TEST(Node, child_NodeIsNullptr)
+{
+	general_tree<int>::node node;
+	EXPECT_THROW(node.child(0), std::invalid_argument);
+}
+
+TEST(Node, child_IndexZeroReturnsTheFirstChild)
+{
+	general_tree<int> test_tree(1);
+	auto first_child = test_tree.insert_left_child(test_tree.root(), 2);
+	test_tree.insert_right_sibling(first_child, 3);
+
+	EXPECT_EQ(test_tree.root().child(0), first_child);
+}
+
+TEST(Node, child_ReturnTheLastChild)
+{
+	general_tree<int> test_tree(1);
+	general_tree<int>::node n = test_tree.root();
+
+	auto first_child = test_tree.insert_left_child(test_tree.root(), 2);
+	auto second_child = test_tree.insert_right_sibling(first_child, 3);
+	auto third_child = test_tree.insert_right_sibling(second_child, 4);
+
+	const std::size_t index = test_tree.children_count(test_tree.root());
+
+	EXPECT_EQ(n.child(index - 1), third_child);
+}
+
+TEST(Node, child_ReturnNullNodeIfNodeDoesNotHaveChildrenAndIndexIsZero)
+{
+	general_tree<int> test_tree(1);
+	general_tree<int>::node n = test_tree.root();
+	const std::size_t index = 0;
+
+	EXPECT_TRUE(n.child(index).is_null());
+}
+
+TEST(Node, child_ThrowOutOfRangeExceptionIfNodeDoesNotHaveChildrenAndIndexIsNotZero)
+{
+	general_tree<int> test_tree(1);
+	general_tree<int>::node n = test_tree.root();
+	const std::size_t index = 1;
+
+	EXPECT_THROW(n.child(index), std::out_of_range);
+}
+
+TEST(Node, child_ShouldReturnNullNodeIfTheSearchedNodeIsTheOneAfterTheLastChild)
+{
+	general_tree<int> test_tree(1);
+	general_tree<int>::node n = test_tree.root();
+
+	auto first_child = test_tree.insert_left_child(test_tree.root(), 2); // 0
+	auto second_child = test_tree.insert_right_sibling(first_child, 3); // 1
+	auto third_child = test_tree.insert_right_sibling(second_child, 4); // 2
+
+	const std::size_t index = 3;
+
+	EXPECT_TRUE(n.child(index).is_null());
 }
