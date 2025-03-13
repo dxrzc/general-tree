9a05dfdd02a277be7dedcd32e9092d6bac67b842
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-13 13:09:51 -0500

    test: move semantics in emplace_left_child method

---
+++ testing/move_semantics/emplace_left_child.ms.test.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include "general-tree.h"
+#include "fixtures/move_semantics.fixture.h"
+
+TEST_F(move_semantics_fixture, emplace_left_child)
+{
+	general_tree<test_resource> tree;
+	tree.create_root(test_resource("", 0));
+	test_resource::reset_calls_count();
+
+	tree.emplace_left_child(tree.root(), "", 1);	
+	
+	EXPECT_FALSE(tree.root().left_child().is_null());
+	EXPECT_EQ(test_resource::instances_created, 1);
+	EXPECT_EQ(test_resource::copy_constructor_calls, 0);
+	EXPECT_EQ(test_resource::move_constructor_calls, 0);
+}
+
+TEST_F(move_semantics_fixture, emplace_left_child_SendTheFullObjectShouldAlsoWork)
+{
+	general_tree<test_resource> tree;
+	tree.create_root(test_resource("", 0));
+	test_resource::reset_calls_count();
+
+	test_resource resource("", 1);
+	tree.emplace_left_child(tree.root(), std::move(resource));
+
+	EXPECT_EQ(test_resource::instances_created, 1);
+	EXPECT_EQ(test_resource::copy_constructor_calls, 0);
+	EXPECT_EQ(test_resource::move_constructor_calls, 1);
+}
