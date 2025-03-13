823ea62a1db3d9e9f690cb67fa088b30304ab561
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-13 13:07:39 -0500

    test: move semantics in constructor

---
+++ testing/move_semantics/constructor.ms.test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "general-tree.h"
+#include "fixtures/move_semantics.fixture.h"
+
+TEST_F(move_semantics_fixture, constructor_ByCopy)
+{
+	test_resource resource("", 0);
+	general_tree<test_resource> tree(resource);
+
+	EXPECT_EQ(test_resource::instances_created, 2);
+	EXPECT_EQ(test_resource::copy_constructor_calls, 1);
+	EXPECT_EQ(test_resource::move_constructor_calls, 0);
+}
+
+TEST_F(move_semantics_fixture, constructor_ByMov)
+{
+	test_resource resource("", 0);
+	general_tree<test_resource> tree(std::move(resource));
+
+	EXPECT_EQ(test_resource::instances_created, 1);
+	EXPECT_EQ(test_resource::copy_constructor_calls, 0);
+	EXPECT_EQ(test_resource::move_constructor_calls, 1);
+}
