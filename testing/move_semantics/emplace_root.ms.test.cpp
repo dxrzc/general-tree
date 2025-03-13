8081ae9ead1b6ae2392748e6d147809b18595e55
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-13 13:09:37 -0500

    test: move semantics in emplace_root method

---
+++ testing/move_semantics/emplace_root.ms.test.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "general-tree.h"
+#include "fixtures/move_semantics.fixture.h"
+
+TEST_F(move_semantics_fixture, emplace_root)
+{
+	general_tree<test_resource> tree;
+
+	tree.emplace_root("", 0);
+
+	EXPECT_FALSE(tree.root().is_null());
+
+	EXPECT_EQ(test_resource::instances_created, 1);
+	EXPECT_EQ(test_resource::copy_constructor_calls, 0);
+	EXPECT_EQ(test_resource::move_constructor_calls, 0);
+}
+
+TEST_F(move_semantics_fixture, emplace_root_SendTheFullObjectShouldAlsoWork)
+{
+	general_tree<test_resource> tree;
+	test_resource resource("", 0);
+
+	tree.emplace_root(std::move(resource));
+
+	EXPECT_EQ(test_resource::instances_created, 1);
+	EXPECT_EQ(test_resource::copy_constructor_calls, 0);
+	EXPECT_EQ(test_resource::move_constructor_calls, 1);
+}
