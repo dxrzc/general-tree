3c39b31f8ca085cd02b944b361a04f9cfa28efaa
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-14 16:09:35 -0500

    chore: move test_resource class to another file

--- testing/helpers/source/test_resource.helper.cpp
+++ testing/helpers/source/test_resource.helper.cpp
@@ -1,4 +1,4 @@
-#include "move_semantics.fixture.h"
+#include "test_resource.helper.h"
 
 thread_local unsigned test_resource::instances_created = 0;
 thread_local unsigned test_resource::move_constructor_calls = 0;
