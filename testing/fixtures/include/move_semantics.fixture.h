98180b0c8082fa4abfa857f98a8810342c3e5bd2
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-15 16:18:16 -0500

    chore: call reset_calls_count in move_semantics_fixture setup

--- testing/fixtures/include/move_semantics.fixture.h
+++ testing/fixtures/include/move_semantics.fixture.h
@@ -6,9 +6,6 @@ class move_semantics_fixture : public testing::Test
 {
 	void SetUp() override
 	{
-		test_resource::instances_created = 0;
-		test_resource::copy_constructor_calls = 0;
-		test_resource::move_constructor_calls = 0;
-		test_resource::destructor_calls = 0;
+		test_resource::reset_calls_count();
 	}
 };
