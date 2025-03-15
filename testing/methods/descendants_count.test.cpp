5c67e751b32032cc10a478bf551790960181d82d
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-15 16:21:07 -0500

    chore: add block to avoid warning caused by "nodiscard" in descendants_count test

--- testing/methods/descendants_count.test.cpp
+++ testing/methods/descendants_count.test.cpp
@@ -5,7 +5,11 @@ TEST(descendants_count, throwInvalidArgumentIfNodeIsNull)
 {
 	general_tree<int> empty_tree;
 	general_tree<int>::node null_node = empty_tree.root();
-	EXPECT_THROW(empty_tree.descendants_count(null_node), std::invalid_argument);
+	EXPECT_THROW({
+		auto dc = empty_tree.descendants_count(null_node);
+	}, 
+	std::invalid_argument
+	);
 }
 
 TEST(descendants_count, returnZeroIfNodeIsALeaf)
