d5f3c4cfe5e64f0573c7b53f112189c6ad64f49f
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-14 19:41:29 -0500

    fix: remove clear method

--- general-tree.h
+++ general-tree.h
@@ -142,11 +142,6 @@ public:
 		);
 	}
 
-	~general_tree()
-	{
-		clear(m_root);
-	}
-
 	template<typename ...Args>
 	node emplace_left_child(node destiny, Args&& ...args)
 	{
@@ -321,17 +316,6 @@ public:
 			total += descendants_count(n) + 1;
 
 		return total;
-	}
-
-	// TODO: test
-	void clear(node n)
-	{
-		if (n.m_node != nullptr)
-		{
-			clear(n.m_node->m_left_child);
-			clear(n.m_node->m_right_sibling);
-			delete n.m_node;
-		}
 	}
 
 	[[nodiscard]] node root() const noexcept
