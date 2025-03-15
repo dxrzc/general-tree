fabdfcf9a685f88bf4d4a0a7ca47239ab4182de7
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-15 16:16:56 -0500

    test: clear method and destructor

---
+++ testing/methods/clear.test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include "general-tree.h"
+
+TEST(clear, noFatalFailureIfItsCalledBeforeDestructor)
+{
+	EXPECT_NO_FATAL_FAILURE({
+		general_tree<int> tree;
+		tree.create_root(1);
+		tree.insert_left_child(tree.root(), 3);
+		tree.insert_left_child(tree.root(), 2);
+		tree.insert_left_child(tree.root().left_child(), 1000);
+		tree.clear();		
+	});
+}
+
+TEST(clear, callOnEmptyTreeDoesNotModifyTheNullRoot)
+{
+	general_tree<int> tree;
+	tree.clear();
+	EXPECT_TRUE(tree.root().is_null());
+}
---
+++ testing/methods/destructor.test.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include "general-tree.h"
+#include "test_resource.helper.h"
+
+class destructor : public ::testing::Test
+{
+	void SetUp() override
+	{
+		test_resource::reset_calls_count();
+	}
+};
+
+TEST_F(destructor, noFatalFailureIfTreeIsEmpty)
+{
+	EXPECT_NO_FATAL_FAILURE({
+		general_tree<int> test_tree;
+	});
+}
+
+TEST_F(destructor, ifTreeContainsOneElementThenDestroyOneElement)
+{
+	{
+		general_tree<test_resource> test_tree;
+		test_tree.emplace_root("root", 1);
+	}
+	EXPECT_EQ(test_resource::destructor_calls, 1);
+}
+
+TEST_F(destructor, deleteAllNodes)
+{
+	{
+		general_tree<test_resource> test_tree;
+		auto root = test_tree.emplace_root("root", 1);
+		auto child1 = test_tree.emplace_left_child(root, "child1", 2);
+		auto child2 = test_tree.emplace_left_child(root, "child2", 3);
+		auto child3 = test_tree.emplace_left_child(child1, "child3", 4);
+		auto child4 = test_tree.emplace_left_child(child2, "child4", 5);
+	}
+	EXPECT_EQ(test_resource::destructor_calls, 5);
+}
