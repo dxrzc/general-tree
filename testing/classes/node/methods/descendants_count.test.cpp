dcb24319d21e049e9019b5847860a7eddd65840d
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-25 21:56:25 -0500

    test: update descendants_count test

--- testing/classes/node/methods/descendants_count.test.cpp
+++ testing/classes/node/methods/descendants_count.test.cpp
@@ -17,10 +17,23 @@ TEST(class_node, descendants_count_returnZeroIfNodeIsALeaf)
 	general_tree<int> empty_tree(1);
 	general_tree<int>::node root = empty_tree.root();
 
-	EXPECT_EQ(empty_tree.root().descendants_count(), 0);
+	EXPECT_EQ(root.descendants_count(), 0);
 }
 
 TEST(class_node, descendants_count_returnTheNumberOfDescendants)
 {
-	// TODO	
+	general_tree<int> tree(0);
+	tree.insert_left_child(tree.root(), 3);
+	tree.insert_left_child(tree.root(), 2);
+	tree.insert_left_child(tree.root(), 1);
+	tree.insert_left_child(tree.root().left_child(), 3);
+	tree.insert_left_child(tree.root().left_child(), 2);
+	tree.insert_left_child(tree.root().left_child(), 1);
+	tree.insert_left_child(tree.root().left_child().right_sibling(), 3);
+	tree.insert_left_child(tree.root().left_child().right_sibling(), 2);
+	tree.insert_left_child(tree.root().left_child().right_sibling(), 1);	
+
+	general_tree<int>::node root = tree.root();
+	
+	EXPECT_EQ(root.descendants_count(), 9);
 }
