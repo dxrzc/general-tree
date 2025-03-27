657a226e4b1659e9069e4af70c107cfa641a7bb4
dxrzc <diegorodriguezc.dev@gmail.com>
2025-03-26 20:25:18 -0500

    fix: add missing populated tree updated file

--- testing/helpers/include/populated_tree.helper.h
+++ testing/helpers/include/populated_tree.helper.h
@@ -1,10 +1,13 @@
 ﻿#pragma once
 #include <general-tree.h>
+#include <vector>
 
 class populated_tree
 {
 public:
 	static inline general_tree<int> tree;
+	static inline std::vector<int> preorder_vector = { 1, 30, 33, 32, 31, 311, 20, 203, 202, 201, 10, 102, 101 };
+	static inline std::vector<int> postorder_vector = { 33, 32, 311, 31, 30, 203, 202, 201, 20, 102, 101, 10, 1 };	
 
 	class builder
 	{
